6c32c1ab5141da4f74c2dc14b3577e45fd71f870
DaRender <liambrynard45@gmail.com>
2026-05-27 12:38:54 -0600

    Refactor master_lst to use a dictionary for packages
    Changed master_lst from a list to a dictionary to include additional package metadata.

--- brew/archive/pkglists.py
+++ brew/archive/pkglists.py
@@ -1,5 +1,10 @@
 # THIS FILE IS MADE FOR JBREW 0.1.0
-master_lst = [
-  "test.py"
-]
+master_lst = {
+  "test.py": {
+      "name": "test.py",
+      "desc": "The first JBrew Package",
+      "author": "solhost",
+      "id": "<%;<.]3"
+  }
+}
 # pkglist version "Breaking Bags"
